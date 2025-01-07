1486cb78918b18bd11ea00ca7114f50b22493d80
Tomas Kallup <t.kallup@gmail.com>
2025-01-07 09:56:02 +0100

    Remove devenv

--- devenv.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, lib, config, inputs, ... }:
-
-{
-  packages = [ pkgs.gnumake inputs.c3c.outputs.packages.${pkgs.system}.c3c ];
-
-  # See full reference at https://devenv.sh/reference/options/
-}
